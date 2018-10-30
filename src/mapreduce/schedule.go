09f32f77455f38289045b980cad4d5dc5730cd7e
JoeXu <joe.0x01@gmail.com>
2018-10-30 23:16:13 +0800

    Lab-1 mapreduce:  part 3 done

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,8 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -30,5 +32,59 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+
+	done := make(chan int, 5)
+	switch phase {
+	case mapPhase:
+		for i, file := range mapFiles {
+
+			go func(i int, file string) {
+
+				addr := <-registerChan
+
+				// TODO: retry on failure
+				_ = call(addr, "Worker.DoTask", DoTaskArgs{
+					JobName:       jobName,
+					File:          file,
+					Phase:         phase,
+					TaskNumber:    i,
+					NumOtherPhase: n_other,
+				}, nil)
+
+				done <- i
+				registerChan <- addr
+
+			}(i, file)
+
+		}
+	case reducePhase:
+
+		for i := 0; i < ntasks; i++ {
+
+			go func(i int) {
+
+				addr := <-registerChan
+
+				// TODO: retry on failure
+				_ = call(addr, "Worker.DoTask", DoTaskArgs{
+					JobName:       jobName,
+					Phase:         phase,
+					TaskNumber:    i,
+					NumOtherPhase: n_other,
+				}, nil)
+
+				done <- i
+				registerChan <- addr
+
+			}(i)
+
+		}
+	}
+
+	// wait for done
+	for i := 0; i < ntasks; i++ {
+		<-done
+	}
+
 	fmt.Printf("Schedule: %v done\n", phase)
 }
