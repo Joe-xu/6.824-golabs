703fe18eee3351cfcd8933a3c45debee1e02f20e
Joe Xu <joe.0x01@gmail.com>
2018-10-31 09:25:46 +0800

    Lab-1 mapreduce: part 4 done

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -2,6 +2,8 @@ package mapreduce
 
 import (
 	"fmt"
+	"log"
+	"sync"
 )
 
 //
@@ -33,58 +35,72 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	// Your code here (Part III, Part IV).
 	//
 
-	done := make(chan int, 5)
+	var taskWg sync.WaitGroup
+	taskChan := make(chan DoTaskArgs, 5)
+
+	taskWg.Add(ntasks)
+	go dispatchTask(taskChan, registerChan, &taskWg)
+
 	switch phase {
 	case mapPhase:
 		for i, file := range mapFiles {
 
-			go func(i int, file string) {
-
-				addr := <-registerChan
-
-				// TODO: retry on failure
-				_ = call(addr, "Worker.DoTask", DoTaskArgs{
-					JobName:       jobName,
-					File:          file,
-					Phase:         phase,
-					TaskNumber:    i,
-					NumOtherPhase: n_other,
-				}, nil)
-
-				done <- i
-				registerChan <- addr
-
-			}(i, file)
+			taskChan <- DoTaskArgs{
+				JobName:       jobName,
+				File:          file,
+				Phase:         phase,
+				TaskNumber:    i,
+				NumOtherPhase: n_other,
+			}
 
 		}
 	case reducePhase:
 
 		for i := 0; i < ntasks; i++ {
 
-			go func(i int) {
-
-				addr := <-registerChan
-
-				// TODO: retry on failure
-				_ = call(addr, "Worker.DoTask", DoTaskArgs{
-					JobName:       jobName,
-					Phase:         phase,
-					TaskNumber:    i,
-					NumOtherPhase: n_other,
-				}, nil)
-
-				done <- i
-				registerChan <- addr
-
-			}(i)
+			taskChan <- DoTaskArgs{
+				JobName:       jobName,
+				Phase:         phase,
+				TaskNumber:    i,
+				NumOtherPhase: n_other,
+			}
 
 		}
 	}
 
 	// wait for done
-	for i := 0; i < ntasks; i++ {
-		<-done
-	}
+	taskWg.Wait()
+	// shutdown dispatcher
+	close(taskChan)
 
 	fmt.Printf("Schedule: %v done\n", phase)
+}
+
+func dispatchTask(tasks chan DoTaskArgs, registerChan chan string, taskWg *sync.WaitGroup) {
+
+	log.Println("dispatch start")
+	for task := range tasks {
+
+		go func(t DoTaskArgs) {
+
+			addr := <-registerChan
+
+			ok := call(addr, "Worker.DoTask", t, nil)
+
+			if !ok {
+				// retry
+				tasks <- t
+			} else {
+				// done
+				taskWg.Done()
+			}
+
+			// free worker
+			registerChan <- addr
+
+		}(task)
+
+	}
+	log.Println("dispatch exit")
+
 }
